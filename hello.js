d5778334b89ae3ab22d1614e13688ae9ecad7af2
leenap30 <82202830+leenap30@users.noreply.github.com>
2021-06-09 17:07:17 +0530

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,12 @@
+exports.handler = async (event) => {
+   
+    var urls = ["https://www.youtube.com/embed/kCl9j9POsJ4",
+                "https://www.youtube.com/embed/k_indcC331o",
+                "https://www.youtube.com/embed/BgKDHAz7PWM",
+                "https://www.youtube.com/embed/SGhEhJVc3SU"];
+     const response = {
+        statusCode: 200,
+        body: JSON.stringify(urls),
+    };
+    return response;
+};
