b2c6ef7f19cd194011ef2c7dbe35d0676d430bf2
leenap30 <82202830+leenap30@users.noreply.github.com>
2021-06-10 11:36:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,29 +49,75 @@ EOF
 }
 
 #API gateway trigger
-resource "aws_apigatewayv2_api" "apiLambda" {
-  name        = "myAPI"
-  protocol_type = "HTTP"
+resource "aws_api_gateway_resource" "proxy" {
+   rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+   parent_id   = aws_api_gateway_rest_api.apiLambda.root_resource_id
+   path_part   = "{proxy+}"
 }
 
- resource "aws_apigatewayv2_integration" "example" {
-  api_id           = aws_apigatewayv2_api.apiLambda.id
-  integration_type = "AWS"
-
-  connection_type           = "INTERNET"
-  content_handling_strategy = "CONVERT_TO_TEXT"
-  description               = "Lambda example"
-  integration_method        = "POST"
-  passthrough_behavior      = "WHEN_NO_MATCH"
+resource "aws_api_gateway_method" "proxyMethod" {
+   rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
+   resource_id   = aws_api_gateway_resource.proxy.id
+   http_method   = "ANY"
+   authorization = "NONE"
 }
 
-resource "aws_apigatewayv2_deployment" "example" {
-  api_id      = aws_apigatewayv2_route.apiLambda.api_id
-  description = "Example deployment"
+resource "aws_api_gateway_integration" "lambda" {
+   rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+   resource_id = aws_api_gateway_method.proxyMethod.resource_id
+   http_method = aws_api_gateway_method.proxyMethod.http_method
+
+   integration_http_method = "POST"
+   type                    = "AWS_PROXY"
+   uri                     = aws_lambda_function.myLambda.invoke_arn
 }
 
-  output "base_url" {
-  value = "echo ${aws_api_gateway_deployment.example.invoke_url} >> op.txt"
+
+
+
+resource "aws_api_gateway_method" "proxy_root" {
+   rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
+   resource_id   = aws_api_gateway_rest_api.apiLambda.root_resource_id
+   http_method   = "ANY"
+   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+   rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+   resource_id = aws_api_gateway_method.proxy_root.resource_id
+   http_method = aws_api_gateway_method.proxy_root.http_method
+
+   integration_http_method = "POST"
+   type                    = "AWS_PROXY"
+   uri                     = aws_lambda_function.myLambda.invoke_arn
+}
+
+
+resource "aws_api_gateway_deployment" "apideploy" {
+   depends_on = [
+     aws_api_gateway_integration.lambda,
+     aws_api_gateway_integration.lambda_root,
+   ]
+
+   rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+   stage_name  = "test"
+}
+
+
+   resource "aws_lambda_permission" "apigw" {
+   statement_id  = "AllowAPIGatewayInvoke"
+   action        = "lambda:InvokeFunction"
+   function_name = aws_lambda_function.myLambda.function_name
+   principal     = "apigateway.amazonaws.com"
+
+   # The "/*/*" portion grants access from any method on any resource
+   # within the API Gateway REST API.
+   source_arn = "${aws_api_gateway_rest_api.apiLambda.execution_arn}/*/*"
+}
+
+
+output "base_url" {
+  value = aws_api_gateway_deployment.apideploy.invoke_url
 }
 
 #s3 bucket
